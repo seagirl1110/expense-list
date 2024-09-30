2ecadceb5bbdbf3c66bc9357338af71cd3285b9d
Marina Elizarova <seagirl1110@gmail.com>
2024-09-30 14:49:26 +0300

    feat: add notification

--- src/components/expenseForm/index.jsx
+++ src/components/expenseForm/index.jsx
@@ -2,10 +2,21 @@ import styles from './styles.module.css';
 import { useState } from 'react';
 import { addExpenseToList } from '../../redux/slices/expenseSlice';
 import { useDispatch } from 'react-redux';
+import { notification } from 'antd';
 
 function ExpenseForm() {
   const dispatch = useDispatch();
   const [formData, setFormData] = useState({ title: '', amount: '' });
+
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (pauseOnHover) => () => {
+    api.open({
+      message: 'Notification',
+      description: `Add '${formData.title}' with amount ${formData.amount}`,
+      showProgress: true,
+      pauseOnHover,
+    });
+  };
 
   const handleChange = (evt) => {
     setFormData({ ...formData, [evt.target.name]: evt.target.value });
@@ -15,26 +26,33 @@ function ExpenseForm() {
     evt.preventDefault();
     dispatch(addExpenseToList({ ...formData, id: Date.now() }));
     setFormData({ title: '', amount: '' });
+    openNotification(true)();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={formData.title}
-        name="title"
-        onChange={handleChange}
-        type="text"
-        placeholder="title"
-      />
-      <input
-        value={formData.amount}
-        name="amount"
-        onChange={handleChange}
-        type="text"
-        placeholder="amount"
-      />
-      <button>Add expense</button>
-    </form>
+    <>
+      {contextHolder}
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          value={formData.title}
+          name="title"
+          onChange={handleChange}
+          type="text"
+          placeholder="title"
+        />
+        <input
+          required
+          value={formData.amount}
+          name="amount"
+          onChange={handleChange}
+          type="number"
+          placeholder="amount"
+        />
+
+        <button>Add expense</button>
+      </form>
+    </>
   );
 }
 export default ExpenseForm;
